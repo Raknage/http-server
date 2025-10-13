3bd812fa788b2c646baf0dc75ef9028dcf7a06cb
raknage <raknage@raknage.com>
2025-10-13 23:38:27 +0300

    ch1l12 custom handlers

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,15 @@ import express from "express";
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+app.use("/app", express.static("./src/app"));
+
+
+app.get("/healthz", (req, res) => {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.status(200).send("OK")
+});
 
 app.listen(PORT, () => {
   console.log(`Server running in port ${PORT}`);
 });
+
