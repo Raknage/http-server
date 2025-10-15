879c06b48d97b59ecdca10ea91c3af7417bc5d0d
raknage <raknage@raknage.com>
2025-10-16 00:29:58 +0300

    ch2l2 API config

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
 import express from "express";
 import { middlewareLogResponses } from "./app/middleware/log.js";
+import { middlewareMetricsInc } from "./app/middleware/metrics.js";
+import { config } from "./config.js";
 
 const app = express();
 const PORT = 8080;
 
 app.use(middlewareLogResponses);
+app.use("/app", middlewareMetricsInc);
 
 app.use("/app", express.static("./src/app"));
 
 app.get("/healthz", (req, res) => {
   res.set("Content-Type", "text/plain; charset=utf-8");
   res.status(200).send("OK");
+});
+
+app.get("/metrics", (req, res) => {
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.status(200).send(`Hits: ${config.fileserverHits}`);
+});
+
+app.get("/reset", (req, res) => {
+  config.fileserverHits = 0;
+  res.set("Content-Type", "text/plain; charset=utf-8");
+  res.status(200).send(`Hits: ${config.fileserverHits}`);
 });
 
 app.listen(PORT, () => {
