bc8de2e6f24d6fa8f9da64045ad05ac5db3af898
raknage <raknage@raknage.com>
2025-10-15 23:37:48 +0300

    ch2l1 middleware

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import express from "express";
+import { middlewareLogResponses } from "./app/middleware/log.js";
 
 const app = express();
 const PORT = 8080;
 
-app.use("/app", express.static("./src/app"));
+app.use(middlewareLogResponses);
 
+app.use("/app", express.static("./src/app"));
 
 app.get("/healthz", (req, res) => {
   res.set("Content-Type", "text/plain; charset=utf-8");
-  res.status(200).send("OK")
+  res.status(200).send("OK");
 });
 
 app.listen(PORT, () => {
   console.log(`Server running in port ${PORT}`);
 });
-
